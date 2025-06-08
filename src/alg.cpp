0bc26204f6e8a5bd2db07b29686d2a47d3a900c2
ettennad <a.derlukova@mail.ru>
2025-06-08 21:43:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,6 @@
 // Copyright 2022 NNTU-CS
-#include  "tree.h"
+#include "tree.h"
+#include <vector>
 
 std::vector<std::vector<char>> getAllPerms(PMTree& tree) {
     std::vector<std::vector<char>> result;
